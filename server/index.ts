bd80f683a3bd3dae8d0a9e4765c73ae92f892350
xavios <mr.acs.andras@gmail.com>
2020-12-12 00:27:54 +0100

    deploy level 3

--- server/index.ts
+++ server/index.ts
@@ -1,45 +1,51 @@
-require('dotenv').config()
-import express, { Express, Request, Response } from 'express';
-import cors from 'cors';
-import userRoutes from './routes/user.routes';
-import eventRoutes from './routes/event.routes';
-import taskRoutes from './routes/task.routes';
-import eventRegistrationRoutes from './routes/eventRegistration.routes';
-import taskRegistrationRoutes from './routes/taskRegistration.routes';
-import connect from './connect';
-import bodyParser from 'body-parser';
-import path from 'path';
+require("dotenv").config();
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import userRoutes from "./routes/user.routes";
+import eventRoutes from "./routes/event.routes";
+import taskRoutes from "./routes/task.routes";
+import eventRegistrationRoutes from "./routes/eventRegistration.routes";
+import taskRegistrationRoutes from "./routes/taskRegistration.routes";
+import connect from "./connect";
+import bodyParser from "body-parser";
+import path from "path";
 
 const app: Express = express();
 
-const port: string | number = process.env.REACT_APP_BACKEND_PORT;
+let port: string | number = process.env.REACT_APP_BACKEND_PORT;
+
+if (process.env.NODE_ENV === "production") {
+  // In prod the PORT means the Heroku port,
+  // while in Dev CRA it menas the frontend port...
+  port = process.env.PORT;
+}
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use(cors());
-app.use('/users', userRoutes);
-app.use('/events', eventRoutes);
-app.use('/tasks', taskRoutes);
-app.use('/eventregistrations', eventRegistrationRoutes);
-app.use('/taskregistrations', taskRegistrationRoutes);
+app.use("/users", userRoutes);
+app.use("/events", eventRoutes);
+app.use("/tasks", taskRoutes);
+app.use("/eventregistrations", eventRegistrationRoutes);
+app.use("/taskregistrations", taskRegistrationRoutes);
 
-app.use(express.static(path.join(__dirname, "../build")))
-app.get("*", function(req, resp) {
-  resp.sendFile(path.join(__dirname, "../build", "index.html"))
-})
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", function (req, resp) {
+  resp.sendFile(path.join(__dirname, "../build", "index.html"));
+});
 
-app.get('/', (req: Request, res: Response) =>
-  res.send('Welcome to the unicef app')
+app.get("/", (req: Request, res: Response) =>
+  res.send("Welcome to the unicef app")
 );
 
 app.listen(port, () =>
   console.log(`Application started successfully on port ${port}.`)
 );
 
-const db = process.env.MONGO_CONNECTION_STRING
+const db = process.env.MONGO_CONNECTION_STRING;
 
-connect({db});
+connect({ db });
