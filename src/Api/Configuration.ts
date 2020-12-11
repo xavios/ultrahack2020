056e626ae0da6a60496006ebed8b90e28655e293
xavios <mr.acs.andras@gmail.com>
2020-12-12 00:50:32 +0100

    Fix backend - frontend conenction

--- src/Api/Configuration.ts
+++ src/Api/Configuration.ts
@@ -1,3 +1,3 @@
 export default class Configuration {
-    public static serviceBaseUrl: string = `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_BACKEND_PORT}`;
+  public static serviceBaseUrl: string = `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_BACKEND_PORT}`;
 }
