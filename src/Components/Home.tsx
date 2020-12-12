fe718cc0942377e636b6d65a6a21f5bbcccaf8b9
Zoltan Bonus <zoltan.bonus@gmail.com>
2020-12-12 11:56:01 +0100

    Fill up home page

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,10 +1,11 @@
 import React, { FC, ReactElement } from "react";
 
+import homeImage from '../karacsony2020_web-hero-image.jpg';
+
 const Home : FC = (): ReactElement => {    
     return (
-        <div className="alert alert-primary alert-success" role="alert">
-            This is the home page!
-        </div>
+        <img src={homeImage} className="img-fluid" alt="Home page" />
+        
     ) 
  };
  
